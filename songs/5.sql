3fb817bd96ebef37481c40c56318473938cee4ec
Toktar Kametay <167887395+InfectedDuck@users.noreply.github.com>
2024-08-02 09:33:35 +0600

    Update 5.sql

--- songs/5.sql
+++ songs/5.sql
@@ -1,2 +1,3 @@
-SELECT AVG(energy)
-FROM songs;
+-- Calculate the average energy level of all songs in the 'songs' table
+SELECT AVG(energy)  -- Computes the average value of the 'energy' column
+FROM songs;  -- Specifies the 'songs' table as the source of data
