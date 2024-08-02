93daae5928c75b6781f67c5f1e5bd222ca45c101
Toktar Kametay <167887395+InfectedDuck@users.noreply.github.com>
2024-08-02 09:33:15 +0600

    Update 4.sql

--- songs/4.sql
+++ songs/4.sql
@@ -1,3 +1,6 @@
-SELECT name
-FROM songs
-WHERE danceability>0.75 AND energy>0.75 AND valence>0.75;
+-- Retrieve the names of songs from the 'songs' table that have high danceability, energy, and valence
+SELECT name  -- Selects the 'name' column from the 'songs' table
+FROM songs  -- Specifies the 'songs' table as the source of data
+WHERE danceability > 0.75  -- Filters songs to include only those with danceability greater than 0.75
+AND energy > 0.75  -- Further filters songs to include only those with energy greater than 0.75
+AND valence > 0.75;  -- Further filters songs to include only those with valence greater than 0.75
