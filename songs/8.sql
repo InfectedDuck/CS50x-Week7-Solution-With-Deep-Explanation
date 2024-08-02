58cb034e93fb8174fd7d986ee9fb053cc0f7b29c
Toktar Kametay <167887395+InfectedDuck@users.noreply.github.com>
2024-08-02 09:34:31 +0600

    Update 8.sql

--- songs/8.sql
+++ songs/8.sql
@@ -1,3 +1,4 @@
-SELECT name
-FROM songs
-WHERE name LIKE "%feat%";
+-- Retrieve the names of songs that include the term "feat" in their title
+SELECT name  -- Selects the 'name' column from the 'songs' table
+FROM songs  -- Specifies the 'songs' table as the source of data
+WHERE name LIKE "%feat%";  -- Filters results to include only songs with "feat" in their title
