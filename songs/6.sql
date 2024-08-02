6b05ab5c1574c50ba2868a532eda9d38fdaff5fe
Toktar Kametay <167887395+InfectedDuck@users.noreply.github.com>
2024-08-02 09:33:52 +0600

    Update 6.sql

--- songs/6.sql
+++ songs/6.sql
@@ -1,3 +1,8 @@
-SELECT name
-FROM songs
-WHERE artist_id = (SELECT id FROM artists WHERE name="Post Malone");
+-- Retrieve the names of songs by the artist named "Post Malone"
+SELECT name  -- Selects the 'name' column from the 'songs' table
+FROM songs  -- Specifies the 'songs' table as the source of data
+WHERE artist_id = (  -- Filters songs to include only those by a specific artist
+    SELECT id  -- Retrieves the 'id' of the artist from the 'artists' table
+    FROM artists  -- Specifies the 'artists' table as the source of data for the subquery
+    WHERE name = "Post Malone"  -- Filters the artist to match the name "Post Malone"
+);
