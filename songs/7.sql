f7c755f5a8de9977cf164ae8a17ccb6de8193fb5
Toktar Kametay <167887395+InfectedDuck@users.noreply.github.com>
2024-08-02 09:34:12 +0600

    Update 7.sql

--- songs/7.sql
+++ songs/7.sql
@@ -1,4 +1,5 @@
-SELECT AVG(energy)
-FROM songs
-JOIN artists ON songs.artist_id=artists.id
-WHERE artists.name="Drake";
+-- Calculate the average energy level of all songs by the artist named "Drake"
+SELECT AVG(energy)  -- Computes the average value of the 'energy' column
+FROM songs  -- Specifies the 'songs' table as the primary source of data
+JOIN artists ON songs.artist_id=artists.id  -- Joins 'songs' table with 'artists' table using a common column
+WHERE artists.name = "Drake";  -- Filters results to include only songs by the artist "Drake"
