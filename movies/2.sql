b41661dfff2abb206a3655531c80ef36f4b352e6
Toktar Kametay <167887395+InfectedDuck@users.noreply.github.com>
2024-08-02 09:29:32 +0600

    Update 2.sql

--- movies/2.sql
+++ movies/2.sql
@@ -1,3 +1,4 @@
-SELECT birth
-FROM people
-WHERE name='Emma Stone';
+-- Retrieve the birth year of a person named 'Emma Stone'
+SELECT birth  -- Selects the 'birth' column from the 'people' table
+FROM people  -- Specifies the 'people' table as the source of data
+WHERE name = 'Emma Stone';  -- Filters the rows to include only those where the 'name' column matches 'Emma Stone'
