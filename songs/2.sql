cf4d4fcac823b83f02a32fcda8c86a176ce6288d
Toktar Kametay <167887395+InfectedDuck@users.noreply.github.com>
2024-08-02 09:32:38 +0600

    Update 2.sql

--- songs/2.sql
+++ songs/2.sql
@@ -1,1 +1,4 @@
-SELECT name FROM songs ORDER BY tempo;
+-- Retrieve the names of all songs from the 'songs' table, ordered by their tempo
+SELECT name  -- Selects the 'name' column from the 'songs' table
+FROM songs  -- Specifies the 'songs' table as the source of data
+ORDER BY tempo;  -- Orders the results by the 'tempo' column in ascending order
