05939a0494f794ce892d1859c059da94c9d9915d
Toktar Kametay <167887395+InfectedDuck@users.noreply.github.com>
2024-08-02 09:29:55 +0600

    Update 3.sql

--- movies/3.sql
+++ movies/3.sql
@@ -1,4 +1,5 @@
-SELECT title
-FROM movies
-WHERE year>=2018
-ORDER BY title;
+-- Retrieve the titles of movies released from 2018 onwards and sort them alphabetically
+SELECT title  -- Selects the 'title' column from the 'movies' table
+FROM movies  -- Specifies the 'movies' table as the source of data
+WHERE year >= 2018  -- Filters the rows to include only those where the 'year' is 2018 or later
+ORDER BY title;  -- Sorts the result set in ascending alphabetical order by the 'title' column
